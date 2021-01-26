3ba4ac8c2f0ccf05b4ef327fe4fa415c5897c811
David Sanchez <david.sanchez@kueski.com>
2021-01-26 11:14:19 -0600

    Sustainable Rails routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :widgets, only: [:index, :show]
+  resources :widget_ratings, only: [:create]
+
+  namespace :customer_service do
+    resources :widgets, only: [:show, :update, :destroy]
+  end
+
+  ####
+  # Custom routes start here →#
+  # For each new custom route: →#
+  # * Be sure you have the canonical route declared above
+  # * Add the new custom route below the existing ones
+  # * Document why it's needed
+  # * Explain anything else non-standard →
+  # Used in podcast ads for the 'amazing' campaign
+  get '/amazing', to: 'widgets#index'
+  get 'manufacturer/:id', to: 'manufacturers#show'
 end
